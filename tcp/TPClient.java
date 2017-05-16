e1d65c939ea242c781bd8489e80f57ac86dfb283
himasha.desilva <himashasds@gmail.com>
2017-05-17 00:21:36 +0530

    tcp udp example

---
+++ tcp/TPClient.java
@@ -0,0 +1,13 @@
+// UDPClient.java: A simple UDP client program.
+
+import java.net.*;
+import java.io.*;
+
+public class TPClient {
+
+    
+
+    public static void main(String args[]) throws UnknownHostException {
+        
+    }
+}
